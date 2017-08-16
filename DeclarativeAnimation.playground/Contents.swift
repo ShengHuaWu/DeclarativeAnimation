6a2af33f00f73b88ef01752f36bfb4b22238e7fa
ShengHua Wu <fantasy0404@gmail.com>
2017-08-16 09:35:19 +0100

    Define animateInParallel method.

--- DeclarativeAnimation.playground/Contents.swift
+++ DeclarativeAnimation.playground/Contents.swift
@@ -5,13 +5,14 @@ let frame = CGRect(x: 0, y: 0, width: 500, height: 500)
 let liveView = UIView(frame: frame)
 liveView.backgroundColor = .white
 
-let smallFrame = CGRect(x: 0, y: 0, width: 50, height: 50)
+let smallFrame = CGRect(x: 200, y: 200, width: 50, height: 50)
 let animationView = UIView(frame: smallFrame)
 animationView.backgroundColor = .red
 animationView.alpha = 0
 liveView.addSubview(animationView)
 
-let size = CGSize(width: 300, height: 300)
-animationView.animate(with: [.fadeIn(), .resize(to: size)])
+let size = CGSize(width: 200, height: 200)
+//animationView.animate([.fadeIn(with: 3.0), .resize(to: size, with: 3.0)])
+animationView.animatieInParallel([.fadeIn(with: 3.0), .resize(to: size, with: 3.0)])
 
 PlaygroundPage.current.liveView = liveView
